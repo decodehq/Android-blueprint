45ca3cb08da0fd836b841d0f8ec8b6a3a8fd4a55
Marko <shark.marko@gmail.com>
2019-06-12 08:45:42 +0200

    Main layout and row layout created

--- app/src/main/java/com/decode/tumblr/App.java
+++ app/src/main/java/com/decode/tumblr/App.java
@@ -5,6 +5,8 @@ import android.app.Application;
 public class App extends Application {
     public static String API_KEY = "si0PHNqV4V39DZyIxV82QFnxovHxcjXsVpzV9mTHlFHQjNivH1";
 
+
+
     @Override
     public void onCreate() {
         super.onCreate();
